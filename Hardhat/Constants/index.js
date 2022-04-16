8c543ffde1f8b688135c8575ff7f5073b5395de2
GOSoccerMaster1 <100545043+GOSoccerMaster1@users.noreply.github.com>
2022-04-15 18:52:36 -0600

    finished project

---
+++ Hardhat/Constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xc7E288b47E524C0894443781CE70af3f7001847a";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
